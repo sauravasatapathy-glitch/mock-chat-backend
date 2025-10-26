2f60f85efca0630949eeacf724d9c85fb549377b
sauravasatapathy-glitch <saurava.satapathy@24-7intouch.com>
2025-10-26 17:37:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-// db.js
-import pkg from 'pg';
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-export default pool;
+module.exports = pool;
